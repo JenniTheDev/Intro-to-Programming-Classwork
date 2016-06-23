cbec6d5917ca989b865a9fd91b5ed3eeddd29511
jenniferafelton <justlittleoldmejenn@gmail.com>
2016-06-23 09:09:18 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+/* 
+ * File:   main.cpp
+ * Author: Jennifer Felton
+ * * Created on June 20, 2016, 6:51 PM
+ * Purpose: First Program
+ */
+
+//system Libraries
+#include <iostream>  //input/output Library
+using namespace std;  //Namespace of the System Libraries
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+
+
+int main(int argc, char** argv) {
+    //Declare Variables
+    
+    //Input Data
+    
+    //Process the Data
+    
+    //Output the processed Data
+    cout<<"Hello World"<<endl;
+    
+    //Exit Stage Right
+    return 0;
+}
+
