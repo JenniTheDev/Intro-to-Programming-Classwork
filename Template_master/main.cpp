9ba3435b0e5ab8cdc69670073ef80db957cd4def
jenniferafelton <justlittleoldmejenn@gmail.com>
2016-07-17 00:33:57 -0700

    woo hoo
    not a bad start!

--- Template_master/main.cpp
+++ Template_master/main.cpp
@@ -1,12 +1,14 @@
 /* 
  * File:   main.cpp
  * Author: Jennifer Felton
- * * Created on June 20, 2016, 6:51 PM
- * Purpose: Template
+ * * Created on July 13, 2016
+ * Purpose: Mastermind Game
  */
 
 //system Libraries
 #include <iostream>  //input/output Library
+#include <ctime>//time for random number seed
+#include <cstdlib> //random number seed
 using namespace std;  //Namespace of the System Libraries
 
 //User Libraries
@@ -20,16 +22,151 @@ using namespace std;  //Namespace of the System Libraries
 
 int main(int argc, char** argv) {
     //Declare Variables
-   
+    int rndm1,rndm2,rndm3,rndm4;//random numbers to turn to colors
+    int nGuess=0, nTries, play;//guesses made, tries needed, play y/n
+    char g1, g2, g3, g4; //user choices of colors
+    char m1,m2,m3,m4;//turn numbers into colors master colors
+    
+    //Set random number seed
+    srand(static_cast<unsigned int>(time(0)));
     
     //Input Data
+    do {
+    cout<<"Welcome to Mastermind!"<<endl;
+    cout<<"To learn how to play, press 1."<<endl;
+    cout<<"To play, press 2"<<endl;
+    cin>>play;
     
+    if (play==1) {
+        cout<<"The objective of the game is to guess the 4 colors,"<<endl<<
+        "in the correct order, that the Codemaker has picked. The Codemaker"<<endl<<
+        "will let you know if you are right or wrong. Good Luck!"<<endl;
+        cout<<endl;
+            } else if (play!=1&&play!=2) {                    //if its not 1 or not 2
+            cout<<"Please pick again"<<endl;
+                } else if (play==2) {
+                    cout<<"Let's play!"<<endl;
+                } }while (play!=2);                //sends back to start with do while loop 
+
+        //Process the Data
+      do {          
+        cout<<"How many tries do you need? Enter a number between 8 and 12"<<endl;
+        cin>>nTries;    
+        if (nTries<=8||nTries>=12) {
+            cout<<"Try again with a number between 8 and 12"<<endl;
+        } if (nTries>=8&&nTries<=12) {
+            cout<<"You have chosen "<<nTries<<endl;
+            cout<<"Good Luck!"<<endl;
+        }
+      } while (nTries<=7&&nTries<=12);  //returns back to number of tries
+      
+      rndm1=rand()%6+1; //randomly generate numbers 
+      rndm2=rand()%6+1; //then equal each number to a letter for a color
+      rndm3=rand()%6+1;
+      rndm4=rand()%6+1;
+      
     
+      if (rndm1==1){    //turns 1st number to color
+          m1='R';               //red
+      } else if (rndm1==2){
+          m1='B';                //blue
+      } else if (rndm1==3){
+          m1='G';                    //green
+      } else if (rndm1==4){
+          m1='Y';                     //yellow
+      } else if (rndm1==5){
+          m1='O';                    //orange
+      }else if (rndm1==6){
+          m1='P';                   //purple
+      } 
     
+    if (rndm2==1){     //turns 2nd number to color
+          m2='R';
+      } else if (rndm2==2){
+          m2='B';
+      } else if (rndm2==3){
+          m2='G';
+      } else if (rndm2==4){
+          m2='Y';        
+      } else if (rndm2==5){
+          m2='O';          
+      }else if (rndm2==6){
+          m2='P';
+      } 
     
-    //Process the Data
+    if (rndm3==1){     //turns 3rd number to color
+          m3='R';
+      } else if (rndm3==2){
+          m3='B';
+      } else if (rndm3==3){
+          m3='G';
+      } else if (rndm3==4){
+          m3='Y';        
+      } else if (rndm3==5){
+          m3='O';          
+      }else if (rndm3==6){
+          m3='P';
+      } 
     
+    if (rndm4==1){    //turns 4th number to color
+          m4='R';
+      } else if (rndm4==2){
+          m4='B';
+      } else if (rndm4==3){
+          m4='G';
+      } else if (rndm4==4){
+          m4='Y';        
+      } else if (rndm4==5){
+          m4='O';          
+      }else if (rndm4==6){
+          m4='P';
+      } 
     
+      cout<<endl;   //some space
+      
+      
+      do{
+      cout<<"Pick 4 colors"<<endl; 
+      cout<<"R,B,G,Y,O, or P"<<endl;
+      cin>>g1>>g2>>g3>>g4;
+      for (nGuess=0;nGuess==nTries ;nGuess++ ){
+      if (g1==m1&&g2==m2&&g3==m3&&g4==m4){
+          cout<<"You won!"<<endl;
+      }
+      
+      if (g1==m1){
+          cout<<"W";
+      } else if (g1!=m1){
+          cout<<"X";
+      }
+      if (g2==m2){
+          cout<<"W";
+      } else if (g2!=m2){
+          cout<<"X";
+      }
+      if (g3==m3){
+          cout<<"W";
+      } else if (g3!=m3){
+              cout<<"X";
+          }
+      if (g4==m4){
+          cout<<"W";
+      } else if (g4!=m4){
+          cout<<"X";
+      }
+      cout<<endl;
+      
+      }
+      }while   (nTries==nGuess);
+      
+      
+      
+      
+      
+      
+      cout<<g1<<"-"<<g2<<"-"<<g3<<"-"<<g4<<endl; //this is for testing delete when done
+      cout<<m1<<"--"<<m2<<"--"<<m3<<"--"<<m4<<endl;//this is for testing delete when done
+      cout<<nGuess<<"guesses should go up"<<endl; 
     //Output the processed Data
     
     
