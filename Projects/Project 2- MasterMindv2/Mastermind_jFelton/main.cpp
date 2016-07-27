498f6e4aad017ddc2f1c22d93c2acb346caf167f
jenniferafelton <justlittleoldmejenn@gmail.com>
2016-07-27 14:10:23 -0700

    progress

--- Projects/Project 2- MasterMindv2/Mastermind_jFelton/main.cpp
+++ Projects/Project 2- MasterMindv2/Mastermind_jFelton/main.cpp
@@ -16,57 +16,114 @@ using namespace std;  //Namespace of the System Libraries
 //Global Constants
 
 //Function Prototypes
+int tries (int);
+char colors (char, char, char, char);
 
 //Execution Begins Here!
 
 
 int main(int argc, char** argv) {
     //Declare Variables
-    int rndm1,rndm2,rndm3,rndm4;//random numbers to turn to colors
+    //int rndm1,rndm2,rndm3,rndm4;//random numbers to turn to colors
     int nGuess=0, nTries, play;//guesses made, tries needed, play y/n
     char g1, g2, g3, g4; //user choices of colors
     char m1,m2,m3,m4;//turn numbers into colors master colors
+  
     
     //Set random number seed
     srand(static_cast<unsigned int>(time(0)));
     
+    //Instructions and into
     
-    //How to play should be one function
-    
-    
-    
-    //Input Data
     do {
         cout<<"Welcome to Mastermind!"<<endl;
         cout<<"To learn how to play, press 1."<<endl;
         cout<<"To play, press 2"<<endl;
         cin>>play;
     
-            if (play==1) {
+        switch(play){
+            case 1: 
                 cout<<"The objective of the game is to guess the 4 colors,"<<endl<<
                 "in the correct order, that the Codemaker has picked. The Codemaker"<<endl<<
                 "will let you know if you are right with a 'O' or wrong with a 'X'."<<endl<<
                 "Good Luck! and Have fun!"<<endl;
-                cout<<endl;
-            } else if (play!=1&&play!=2) {                    //if its not 1 or not 2
-                cout<<"Please pick again"<<endl;
-            } else if (play==2) {
-                cout<<"Let's play!"<<endl;
-                    } 
-        }while (play!=2);                //sends back to start with do while loop 
+                cout<<endl; 
+                break;
+            case 2:   cout<<"Let's play!"<<endl;
+                      break;  
+            default: cout<<"Not a valid answer, please choose 1 for directions or 2 to play"<<endl;break; 
+            } 
+    } while (play !=2);     //If they type in a letter instead of a number, it makes an endless loop. How can I fix this? 
+    
+    
+   tries (nTries); //function for number of tries
+    
+   colors (m1,m2,m3,m4); //function for color convert from numbers to letters
+       
+     
+          //THis is how the game is played. it can be a function
+
 
     
-        
-        
-        
-        
-        //number of tries should be a function 
-        
-        
-        
-        
-        
-    //Process the Data
+    do{
+        cout<<"Pick 4 colors"<<endl; 
+        cout<<"R,B,G,Y,O, or P"<<endl;
+        cin>>g1>>g2>>g3>>g4;
+     
+        if (g1==m1&&g2==m2&&g3==m3&&g4==m4){
+            cout<<"You won!"<<endl;break;
+            } if (g1==m1){
+                cout<<"O";
+            } else if (g1!=m1){
+                cout<<"X";
+            } if (g2==m2){
+                cout<<"O";
+            } else if (g2!=m2){
+                cout<<"X";
+            }if (g3==m3){
+                cout<<"O";
+            } else if (g3!=m3){
+                cout<<"X";
+            }if (g4==m4){
+                cout<<"O";
+            } else if (g4!=m4){
+                cout<<"X";
+            }
+      
+        cout<<endl;
+        nGuess++;
+      
+    }while (nGuess<nTries); 
+    
+    //write a for loop for black/white pegs # of black, # of white, ++ every time 
+    
+    
+    //function here to record guesses and write them to an array? 
+    
+    
+    
+    //exiting the game can be a function
+    
+    
+    cout<<"Game Over"<<endl;
+    cout<<"You tried "<<nGuess<<" times"<<endl;
+    cout<<"The code was "<<m1<<"-"<<m2<<"-"<<m3<<"-"<<m4<<endl;
+    cout<<"Thanks for Playing Mastermind!"<<endl;
+      
+         
+      //I left these here to show how I tested as I went along
+      //cout<<g1<<"-"<<g2<<"-"<<g3<<"-"<<g4<<endl; //this is for testing delete when done
+      //cout<<m1<<"--"<<m2<<"--"<<m3<<"--"<<m4<<endl;//this is for testing delete when done
+      //cout<<nGuess<<"guesses should go up"<<endl; 
+      
+    
+    //Exit Stage Right
+    return 0;
+}
+
+
+int tries (int nTries){
+    
     do {          
         cout<<"How many tries do you need? Enter a number between 8 and 12"<<endl;
         cin>>nTries;    
@@ -76,14 +133,15 @@ int main(int argc, char** argv) {
             cout<<"You have chosen "<<nTries<<endl;
             cout<<"Good Luck!"<<endl;
         }
-    } while (nTries<=7&&nTries<=12);  //returns back to number of tries
-      
+    } while (nTries<=7&&nTries<=12);  
+    return nTries;
+    //returns back to number of tries
     
+}
     
-    
-    
-    //The number to color business should be its own function
-    
+
+char colors (char m1, char m2, char m3, char m4){
+    int rndm1, rndm2, rndm3, rndm4;
     
     rndm1=rand()%6+1; //randomly generate numbers 
     rndm2=rand()%6+1; //then equal each number to a letter for a color
@@ -149,71 +207,5 @@ int main(int argc, char** argv) {
     }else if (rndm4==6){
         m4='P';
     } 
-   
-    cout<<endl;   //some space
-      
-
-
-
-
-
-    //THis is how the game is played. it can be a function
-
-
-    
-    do{
-        cout<<"Pick 4 colors"<<endl; 
-        cout<<"R,B,G,Y,O, or P"<<endl;
-        cin>>g1>>g2>>g3>>g4;
-     
-        if (g1==m1&&g2==m2&&g3==m3&&g4==m4){
-            cout<<"You won!"<<endl;break;
-            } if (g1==m1){
-                cout<<"O";
-            } else if (g1!=m1){
-                cout<<"X";
-            } if (g2==m2){
-                cout<<"O";
-            } else if (g2!=m2){
-                cout<<"X";
-            }if (g3==m3){
-                cout<<"O";
-            } else if (g3!=m3){
-                cout<<"X";
-            }if (g4==m4){
-                cout<<"O";
-            } else if (g4!=m4){
-                cout<<"X";
-            }
-      
-        cout<<endl;
-        nGuess++;
-      
-    }while (nGuess<nTries); 
-    
-    //write a for loop for black/white pegs # of black, # of white, ++ every time 
-    
-    
-    //function here to record guesses and write them to an array? 
-    
-    
-    
-    //exiting the game can be a function
-    
-    
-    cout<<"Game Over"<<endl;
-    cout<<"You tried "<<nGuess<<" times"<<endl;
-    cout<<"The code was "<<m1<<"-"<<m2<<"-"<<m3<<"-"<<m4<<endl;
-    cout<<"Thanks for Playing Mastermind!"<<endl;
-      
-         
-      //I left these here to show how I tested as I went along
-      //cout<<g1<<"-"<<g2<<"-"<<g3<<"-"<<g4<<endl; //this is for testing delete when done
-      //cout<<m1<<"--"<<m2<<"--"<<m3<<"--"<<m4<<endl;//this is for testing delete when done
-      //cout<<nGuess<<"guesses should go up"<<endl; 
-      
-    
-    //Exit Stage Right
-    return 0;
+    return m1,m2,m3,m4;
 }
-
